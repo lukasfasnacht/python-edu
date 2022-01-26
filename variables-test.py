9ecb6bc71de1f7fc5abce1847d576e2def2523b6
Lukas Fasancht <lukas.fasnacht@gmail.com>
2022-01-26 16:58:55 +0100

    created first text with string variables

---
+++ variables-test.py
@@ -0,0 +1,9 @@
+char_name = "John" #String
+char_age = "35.5" #String
+print("There once was a man named " + char_name + ",")
+print("he was " + char_age + " years old.")
+
+char_name = "Mike"
+print("He really liked the name " + char_name + ",")
+print("but didn't like being " + char_age + ".")
+  
