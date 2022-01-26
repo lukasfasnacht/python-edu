8a6bd6d9b67fed640d21ada88094c34c5e2ec19f
Lukas Fasancht <lukas.fasnacht@gmail.com>
2022-01-26 17:20:41 +0100

    created script which manipulates string values

---
+++ strings.py
@@ -0,0 +1,11 @@
+phrase = "Lukas Python Academy"
+
+print(phrase + "\nist awesome!")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.lower().islower())
+print(len(phrase))
+print(phrase[0])
+print(phrase[19])
+print(phrase.index("L"))
+print(phrase.replace("Lukas", "Fasnacht"))
