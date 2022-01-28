c789c7859fea5aa1fb85f10b59ba6d9e9a997381
Lukas Fasancht <lukas.fasnacht@gmail.com>
2022-01-28 01:06:28 +0100

    created a basic list

---
+++ list.py
@@ -0,0 +1,19 @@
+friends = ["Kevin", "Mike", "Josh", "Jim", "Tom"]
+lucky_numbers = [7, 13, 17, 21, 37, 51]
+
+#friends.extend(lucky_numbers)
+#friends.append("Jimmy")
+#friends.insert(0, "Annina")
+#friends.remove("Jim")
+#friends.clear()
+lucky_numbers.reverse()
+
+print(friends)
+print(friends[2:])
+print(friends[-1])
+print(friends[1:3])
+print(friends.index("Kevin"))
+print(friends.count("Annina"))
+
+print(lucky_numbers)
+
