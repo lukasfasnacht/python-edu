ab8de901de5c3ea9c5e837ab6814f6b79441c8ad
Lukas Fasancht <lukas.fasnacht@gmail.com>
2022-01-31 18:41:43 +0100

    added goodbye function

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 def say_hello(name, age):
   print("Hello " + name + ", you are " + str(age))
 
+def say_goodbye():
+  print("Goodbye!")
+
 say_hello("Mike", 35)
 say_hello("Steve", 70)
+say_goodbye()
